9701e2702eb4dbd454fb0a98841f43f45d05c629
jamesmutune <tunekivasu01@gmail.com>
2024-01-17 12:27:24 +0300

    completed assignment

--- README.md
+++ README.md
@@ -1,21 +1,14 @@
-This programm is for creating a movie website which has a a list of movie titles which when clicked shows the poster of the said movie .
+# Flatiron Movie Theater
+## Description
+### The website dispalys movie's details, and their particular details such as  title, runtime, showtime, and available tickets when the page loads. 
 
-TECHNOLOGIES USED
-git
-json
-Project set up
-To run the code you will have to clone the repository into your computer and then in your terminal and then run this:json-server --watch db.json
+# code Structure
+### The HTML file (index.html) contains the structure of the web app.
+### The CSS styling is defined in an external stylesheet (main.css).
+### JavaScript functionality is implemented in the index.js script, which fetches character data from an API and dynamically updates the content.
 
-CODE FEATURES
-In the html there is a div with thee class container that contains two columns which are loaded with content from the javascript file .
+# How to Customize the Code
+## Git clone the SSH key `git@github.com:John-Mwau/Week3-Code-Challenge.git` to your local machine. 
 
-in the javascript file tjere is a function called poster(movie) that has a a div containing the poster,when the movie title is clicked, the poster opens.
-
-when the poster pops up the user will be able to see the movie title, a description and the number of tickets available, if the tickets are over a message will pop up.
-
-how to use the program
-Upon opening the application, you'll see a list of available movies in the sidebar.
-Click on a movie to view its details in the main content area.
-If the tickets are available, a "Buy Ticket" button is displayed. Clicking this button will allow you to buy a ticket.
-Author & license
-Kisengi.
+# Author
+ ## This Movie web app is created by James Kisengi.
--- index.js
+++ index.js
@@ -1,101 +1,136 @@
-const dataLink = `http://localhost:3000/films`; // URL for the JSON server
-
-document.addEventListener('DOMContentLoaded', () => {
-    movieFetch();
-
-    // Function to fetch movie data from the server
-    function movieFetch() {
-        fetch(dataLink)
-            .then(res => res.json())
-            .then(data => {
-                // Calculate available tickets and populate movie list
-                ticketCalculator(data);
-                movielist(data);
-
-                // Add click event to movie list items
-                const listItems = document.querySelectorAll('.list-item');
-                listItems.forEach(listItem => {
-                    listItem.addEventListener('click', () => {
-                        // Find selected movie and display its details
-                        const selectedMovie = data.find(movie => movie.title === listItem.textContent);
-                        poster(selectedMovie);
-                        console.log('Clicked on: ', listItem.textContent);
-                    });
-                });
-            });
-    }
-
-    let availableTickets, capacity, soldTickets;
-
-    // Function to populate the movie list in the sidebar
-    function movielist(data) {
-        const movienamecontainer = document.querySelector('.movienamecontainer');
-        data.forEach(item => {
-            movienamecontainer.innerHTML += `<li class="list-item">${item.title}</li>`;
-        });
-    }
-
-    // Function to calculate ticket availability for each movie
-    function ticketCalculator(data) {
-        data.forEach(item => {
-            capacity = item.capacity;
-            soldTickets = item.tickets_sold;
-            availableTickets = parseInt(capacity - soldTickets);
-            item.availableTickets = availableTickets;
-        });
-    }
-
-    // Function to display movie details in the main content area
-    function poster(movie) {
-        // Create and populate movie card
-        const posterdiv = document.createElement('div');
-        posterdiv.innerHTML = `
-        <div class="card">
-            <div class="card2" style="width: 18rem;">
-                <img src="${movie.poster}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${movie.title}</h5>
-                    <p class="card-text">${movie.description}</p>
-                    <p class="available">Tickets available: ${movie.availableTickets}</p>
-                    <p class="unavailable"></p>
-                    <button class="ticketBuy">Buy Ticket</button>
+/* 
+let films = [] //Define a string variable to hold films data
+document.addEventListener("DOMContentLoaded",()=>{
+    getflims ()
+})
+// Get movies data from the local json server
+let baseURL= "https://my-json-server.typicode.com/John-Mwau/Week3-Code-Challenge/films"
+function getflims (){ 
+    fetch(baseURL)
+    .then (res =>res.json())
+    
+    .then (data=>{
+        films = [...data]
+        dispalyFlims (films)
+        
+    })
+    
+}
+//Function to display all movies within the div. Three Movies are to be displayed in each row.
+function dispalyFlims (films){
+    const filmcontainer = document.querySelector("#film")
+    films.forEach(film=> {     //Get data for each movie from the object and populate it to the respective IDs.
+        filmcontainer.innerHTML += `
+        <div class="p-2 m-3 col-3">
+            <div class="card"id="card" >
+                <div class="card-body" >
+                <img src="${film.poster}" class="card-img-top" alt="${film.description}">
+                <h5 class="card-title">${film.title}</h5>
+                <h6>${film.description}</h6>
+                    <span>
+                    <ul>
+                        <li>Runtime:${film.runtime}</li>
+                    
+                        <li>Showtime:${film.showtime}</li>
+                        
+                    <li>Available Ticket:${film.capacity-film.tickets_sold}</li>
+                        
+                    </ul>
+                    </span>
+                        <form>
+                    <button id= "Buy_Button"> Buy Ticket</button>
+                        </form>
                 </div>
             </div>
-        </div>`    ;
+                
+            </div>`; 
 
-        // Append movie card to the main content area
-        const columndiv = document.querySelector('#columndiv');
-        columndiv.innerHTML = ''; // Clear previous content
-        columndiv.appendChild(posterdiv);
+           filmContainer.appendChild(card);
 
-        // Add click event to Buy Ticket button
-        const buyButton = posterdiv.querySelector('.ticketBuy');
-        buyButton.addEventListener('click', () => {
-            buyTicket(movie);
+        const buyButton = card.querySelector(".buy-button");
+        const availableTickets = card.querySelector(".available-tickets");
+
+        buyButton.addEventListener("click", () => {
+            // Decrease available tickets by 1
+            const newAvailableTickets = parseInt(availableTickets.textContent, 10) - 1;
+            availableTickets.textContent = newAvailableTickets;
         });
-    }
+            
+})
+ }  
+ fetchMovieData();
 
-    // Function to handle ticket purchase logic
-    function buyTicket(ClickedMovie) {
-        if (ClickedMovie.availableTickets > 0) {
-            ClickedMovie.soldTickets++;
-            ClickedMovie.availableTickets--;
-            updateTicketInfo(ClickedMovie);
-        } else if (ClickedMovie.availableTickets < 1) {
-            emptyTicket(ClickedMovie);
-            console.log('Sorry, all tickets for this movie have been sold out!');
-        }
-    }
 
-    // Function to display message when tickets are sold out
-    function emptyTicket(ClickedMovie) {
-        const unavailableTickets = document.querySelector('.unavailable');
-        unavailableTickets.textContent = `Sorry, all tickets for this movie have been sold out`;
-    }
 
-    // Function to update available ticket information in the card
-    function updateTicketInfo(ClickedMovie) {
-        const availableTicketsElement = document.querySelector('.available');
-        availableTicketsElement.textContent = `Tickets available: ${ClickedMovie.availableTickets}`;
-    }
+ // Event listener for the "Buy Ticket" button
+ buyTicketButton.addEventListener("click", () => {
+     const available = parseInt(availableTickets.textContent);
+     if (available > 0) {
+         availableTickets.textContent = available - 1;
+     }
+     fetch(`${baseURL}/${movieId}`, {
+         method: "PUT",
+         headers: {
+             "Content-Type": "application/json",
+         },
+         body: JSON.stringify({ tickets_sold: movie.capacity - updatedCapacity }),
+     })
+ });
+
+  */
+ let films = []; // Define a variable to hold films data
+
+document.addEventListener("DOMContentLoaded", () => {
+    getFilms();
 });
+
+// Get movies data from the local JSON server
+let baseURL = "https://my-json-server.typicode.com/John-Mwau/Week3-Code-Challenge/films";
+
+function getFilms() {
+    fetch(baseURL)
+        .then(res => res.json())
+        .then(data => {
+            films = [...data];
+            displayFilms(films);
+        });
+}
+
+// Function to display movies within the div. Three Movies are to be displayed in each row.
+function displayFilms(films) {
+    const filmContainer = document.querySelector("#film");
+
+    films.forEach(film => {
+        const card = document.createElement("div");
+        card.className = "p-2 m-3 col-3 card";
+        card.innerHTML = `
+            <div class="card-body">
+                <img src="${film.poster}" class="card-img-top" alt="${film.description}">
+                <h5 class="card-title">${film.title}</h5>
+                <h6>${film.description}</h6>
+                <ul>
+                    <li>Runtime: ${film.runtime}</li>
+                    <li>Showtime: ${film.showtime}</li>
+                    <li>Available Tickets: <span class="available-tickets">${film.capacity - film.tickets_sold}</span></li>
+                </ul>
+                <form>
+                    <button class="buy-button">Buy Ticket</button>
+                </form>
+            </div>
+        `;
+        filmContainer.appendChild(card);
+
+        const buyButton = card.querySelector(".buy-button");
+        const availableTickets = card.querySelector(".available-tickets");
+
+        buyButton.addEventListener("click", (e) => {
+            // Decrease available tickets by 1
+            e.preventDefault()
+            const newAvailableTickets = parseInt(availableTickets.textContent, 10) - 1;
+            availableTickets.textContent = newAvailableTickets;
+            if (newAvailableTickets === 0){
+                alert ("This film is sold out!")
+            }
+        });
+    });
+}
